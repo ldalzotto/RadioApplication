4a0ec0c084eb1c14c8c05e3ae0e1abcd773483dc
= <loic.dalzotto@hotmail.fr>
2017-06-05 21:47:17 +0200

    correction sur l'affichage correct des iframes

--- src/main/resources/static/js/music/musicNavigation.js
+++ src/main/resources/static/js/music/musicNavigation.js
@@ -8,7 +8,7 @@ var MusicView = (function () {
   $(document).ready(function(){
     musicPanelListElement = $('#music-navbar-content').find('#music-panel-list');
     musicPanelInfoTemplateElement = $('#music-panel-info');
-    musicPanelInfoTemplateElement.hide();
+    musicPanelInfoTemplateElement.show();
   })
 
   var fromUsername = function(actionToDo){
@@ -22,14 +22,16 @@ var MusicView = (function () {
         method: 'GET',
         url: 'user/' + username + '/music/all',
         success: function (musicManagerType) {
+          musicPanelInfoTemplateElement.show()
           var numberOfMusic = musicManagerType.musicTypeDTO.length
 
           if(numberOfMusic != 0) {
             musicPanelListElement.empty()
             var listMusicType = musicManagerType.musicTypeDTO
             for (i = 0; i < listMusicType.length; i++) {
-              var musicPanelInfo = musicPanelInfoTemplateElement.clone()
+              var musicPanelInfo = musicPanelInfoTemplateElement.clone().removeClass('out-of-bound')
               musicPanelInfo.find('#music-iframe').attr('src', listMusicType[i].sourceUrl)
+              musicPanelInfo.show()
               musicPanelListElement.append(musicPanelInfo)
             }
             musicPanelListElement.show()
@@ -45,7 +47,7 @@ var MusicView = (function () {
 
   return {
     displayMusicPanel: function () {
-      musicPanelListElement.hide()
+      musicPanelListElement.show()
       AddMusicModal.hideModal()
       refreshMusicListPanel()
     }
